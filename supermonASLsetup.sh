9b47b38d97d3b55f8bc82ba0b44082cf3421c807
firealarmss <ko4uyj@gmail.com>
2022-09-02 14:21:59 -0500

    Updated Directorys

--- supermonASLsetup.sh
+++ supermonASLsetup.sh
@@ -11,20 +11,21 @@ sleep 2
 echo "Moved Web Pages"
 sleep 1
 if [ -d "/etc/apache2/conf-available/" ]; then
-  cp -r etc/apache2/conf-available/serve-cgi-bin.conf /etc/apache2/conf-available/serve-cgi-bin.conf
+  cp etc/apache2/conf-available/serve-cgi-bin.conf /etc/apache2/conf-available/serve-cgi-bin.conf
 fi
 sleep 2
 echo "Moved Apache Conf"
 sleep 1
 if [ -d "/usr/local/sbin" ]; then
-  cp -r /usr/local/sbin/ /usr/local/sbin/
+  cp -r usr/local/sbin/ /usr/local/
 fi
 sleep 2
 echo "Moved Sbin files"
 sleep 1
-echo "Now..... go to your ip. e.g. 192.168.1.XXX"
+echo "Now..... go to your ip. e.g. 192.168.1.XXX/supermon"
 sleep 1
 echo "go to https://hamvoip.org/howto/supermon_howto.pdf"
 sleep 1
 echo "directory names may be a bit different but will give an idea of the setup"
 
+You have new mail in /var/mail/repeater
